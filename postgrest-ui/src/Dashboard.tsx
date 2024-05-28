8a7b8bfaba0b0af2f10a6bbc9a9b5d79cdb96055
karbyshevDm <che343925@gmail.com>
2024-05-28 21:30:16 +0300

    update

--- postgrest-ui/src/Dashboard.tsx
+++ postgrest-ui/src/Dashboard.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardHeader } from "@mui/material";
 
 export const Dashboard = () => (
     <Card>
-        <CardHeader title="Welcome to the application Postres Viewer" />
+        <CardHeader title="Welcome to the application Postgres Viewer" />
         <CardContent>Application is designed by ...</CardContent>
     </Card>
 );
